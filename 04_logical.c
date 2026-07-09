d723d75e115a871774154855c8585f0ba66e3d92
HyunKyu_KIM <rlatkddbs2005@gmail.com>
2026-07-09 17:14:52 +0900

    if 조건문에서 괄호 사용 오류

---
+++ 04_logical.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3;
+    printf("num1을 입력해주세요");
+    scanf("%d", &num1);
+    printf("num2을 입력해주세요");
+    scanf("%d", &num2);
+    printf("num3을 입력해주세요");
+    scanf("%d", &num3);
+    if (num1 > num2) && (num1 > num3)
+    {
+        printf("num1이 가장큰 수입니다.");
+    }
+    else 
+    {
+        printf("num1이 가장큰 수가 아닙니다.");
+    }
+    return 0;
+}
