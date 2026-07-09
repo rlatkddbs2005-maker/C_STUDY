9c410409d460e26cf3381a2bb27b7c40e755cda7
HyunKyu_KIM <rlatkddbs2005@gmail.com>
2026-07-09 17:37:41 +0900

    sum을 선언할때 위치를 잘못함. 값이 들어오고 연산해야함을 배우고 연산 순서 공부

--- 04_logical-2.c
+++ 04_logical-2.c
@@ -2,11 +2,11 @@
 int main()
 {
     int num1, num2;
-    int sum = num1 + num2;
     printf("num1을 입력하세요");
     scanf("%d", &num1);
     printf("num2를 입력하세요");
     scanf("%d", &num2);
+    int sum = num1 + num2;
     if (num1 == 2 * num2 || num2 == 2 * num1)
     {
         printf("%d", sum);
