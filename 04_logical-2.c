d11e9df72cd2341bcb4cb06fad28e2faf7fefe2b
HyunKyu_KIM <rlatkddbs2005@gmail.com>
2026-07-09 17:28:04 +0900

    정상 실행은 되는데 sum값을 출력할때 적절한 값이 출력 되지 않음

---
+++ 04_logical-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    int sum = num1 + num2;
+    printf("num1을 입력하세요");
+    scanf("%d", &num1);
+    printf("num2를 입력하세요");
+    scanf("%d", &num2);
+    if (num1 == 2 * num2 || num2 == 2 * num1)
+    {
+        printf("%d", sum);
+    } 
+    else
+    {
+        printf("조건이 충족하지 않습니다.");
+    }
+    return 0;
+}
